57ce45f396cf2d529d01292f1d387f248491fa89
HyunSuk Nam <me@imnyang.xyz>
2024-08-03 22:58:44 +0900

    ?kawaii

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,29 +1,42 @@
 import { useEffect, useState, useMemo } from 'react';  
+import { useLocation } from 'react-router-dom';
 import './Root.css';
 import Help from '../component/help';
 
 function Root() {
-
+  const location = useLocation();
   const name: string[] = useMemo(() => ['imnyang', '아임냥', '암냥', '임냥', '@not.furry_', '@imnyang', '@mahiro_me'], []);
   const [nameIndex, setNameIndex] = useState(0);
+  const [imageSrc, setImageSrc] = useState('https://f.imnyang.xyz/profile/imnyang.webp');
+
   useEffect(() => {
-      const handleKeyDown = (event: KeyboardEvent) => {
-          // 단일 키 입력 감지
-          console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`);
-          if (event.key === 'l') {
-            console.log('Change My Name');
-            setNameIndex((prevIndex) => (prevIndex + 1) % name.length);
-          }
-      };
-          
-      document.addEventListener('keydown', handleKeyDown);
-  
-      return () => {
-          document.removeEventListener('keydown', handleKeyDown);
-      };
-    }, [name.length, name]);
+    const queryParams = new URLSearchParams(location.search);
+    if (queryParams.has('kawaii')) {
+      setImageSrc('https://f.imnyang.xyz/profile/magic_imnyang.webp');
+    } else {
+      setImageSrc('https://f.imnyang.xyz/profile/imnyang.webp');
+    }
+  }, [location.search]);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`);
+      if (event.key === 'l') {
+        console.log('Change My Name');
+        setNameIndex((prevIndex) => (prevIndex + 1) % name.length);
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [name.length, name]);
+
   return (
     <div className='App'>
+      <img src={imageSrc} width={256} className='profile' />
       <h1 style={{color: '#fadfee', fontSize: 60, margin: 0}}>
         {name[nameIndex]}
       </h1>
@@ -43,7 +56,7 @@ function Root() {
       </div>
       <Help />
     </div>
-  )
+  );
 }
 
 export default Root;
